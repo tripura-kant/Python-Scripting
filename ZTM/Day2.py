53c4d9c194e24b767167864c2db37a71289d1517
Tripura kant <tripurakant@gmail.com>
2023-04-24 01:05:34 +0530

    24/4/2023

--- ZTM/Day2.py
+++ ZTM/Day2.py
@@ -1,3 +1,17 @@
-print(4 > 5)
+#print(4 > 5)
 
-print(4 == 5)
+#print(4 == 5)
+
+#Exercise
+
+is_magician = False
+
+is_expert = True
+
+#check if magician and expert : ""you are a master magician"
+
+if is_magician and is_expert:
+    print("you are a master magician")
+else:
+    print("Exit block")
+
