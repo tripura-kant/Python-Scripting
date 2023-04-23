5af4ded969932304b814279a3e61e3f085a2fadf
Tripura kant <tripurakant@gmail.com>
2023-04-24 01:12:09 +0530

    24/4/2023

--- ZTM/Day2.py
+++ ZTM/Day2.py
@@ -1,17 +1,24 @@
-#print(4 > 5)
+# print(4 > 5)
 
-#print(4 == 5)
+# print(4 == 5)
 
-#Exercise
+# Exercise for if-else condition
 
 is_magician = False
 
 is_expert = True
 
-#check if magician and expert : ""you are a master magician"
+# check if magician and expert : "you are a master magician"
 
 if is_magician and is_expert:
     print("you are a master magician")
+
+elif is_magician and not is_expert:
+    print("At lease you getting there")
+
+elif not is_magician:
+    print("You need magic power")
+
 else:
     print("Exit block")
 
