b6f7e721c4471f9422c4237c6565131b990a0057
Tripura kant <tripurakant@gmail.com>
2023-04-25 00:47:11 +0530

    24/4/2023

---
+++ ZTM/Day3.py
@@ -0,0 +1,11 @@
+#
+# for number in range(0, 90, 2):
+#     print(number)
+#
+
+# for i,char in enumerate(list(range(100))):
+#     print(i, char)
+#     if char == 50:
+#         print(f'index of 50 is: {i}')
+
+
