d0e3a2a6f3720790ebd5018d5731c32d3f483eb0
Tripura kant <tripurakant@gmail.com>
2023-04-26 01:26:47 +0530

    26/4/2023

--- ZTM/Day4c.py
+++ ZTM/Day4c.py
@@ -4,9 +4,15 @@ some_list = ['a', 'b', 'c', 'd', 'm', 'n', 'c', 'n']
 
 duplicate = []
 
-for value in some_list:
-    if some_list.count(value) > 1:
-        if value not in duplicate:
-           duplicate.append(value)
 
-print(duplicate)
+def show_tree():
+    for value in some_list:
+        if some_list.count(value) > 1:
+            if value not in duplicate:
+               duplicate.append(value)
+
+    print(duplicate)
+
+show_tree()
+show_tree()
+show_tree()
---
+++ ZTM/Day4e.py
@@ -0,0 +1,12 @@
+#parametes
+
+def say_hello(name, emoji):
+    print(f'helooooo {name} {emoji}')
+
+# arguments
+
+say_hello('monu', '😃')
+
+
+
+
