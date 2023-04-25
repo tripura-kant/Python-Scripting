ee42797c418f68373e5e9bde2e2d5153eddab895
Tripura kant <tripurakant@gmail.com>
2023-04-26 00:34:05 +0530

    24/4/2023

--- ZTM/Day3.py
+++ ZTM/Day3.py
@@ -1,4 +1,4 @@
-#
+#https://www.udemy.com/course/complete-python-developer-zero-to-mastery/learn/lecture/16016662#questions
 # for number in range(0, 90, 2):
 #     print(number)
 #
@@ -9,3 +9,9 @@
 #         print(f'index of 50 is: {i}')
 
 
+i = 0
+while i < 50:
+    print(i)
+    i += 1
+else:
+    print('done with all the work')
---
+++ ZTM/Day4.py
@@ -0,0 +1,15 @@
+# # While Loop 2
+#
+# for item in [1, 2, 3]:
+#     print(item)
+#
+# i = 0
+#
+# while i < len([1,2,3]):
+#     print(i)
+#     i+= 1
+
+# while True:
+#     response = input('say something:')
+#     if (response == 'bye'):
+#         break
