6f0f42ee6ad460172637c3cb94817a46f29ccdc4
Tripura kant <tripurakant@gmail.com>
2023-04-28 23:12:19 +0530

    26/4/2023

---
+++ ZTM/Day6.py
@@ -0,0 +1,9 @@
+def highest_even(li):
+    evens = []
+    for item in li:
+        if item % 2 == 0:
+            evens.append(item)
+            return max (evens)
+print(highest_even([10,12,2,4,11]))
+
+
---
+++ ZTM/Day7.py
@@ -0,0 +1,11 @@
+a = 'helooooo'
+
+if ((n := len(a)) > 10):
+    print(f"too long {n} elemnts")
+
+while (n := len(a) > 1):
+    print(n)
+    a = a[:-1]
+
+print(a)
+
