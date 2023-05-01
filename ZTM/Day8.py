ff91dd48dd21c6cf0b7cbfbb4c99c3e3b1ef5cfa
Tripura kant <tripurakant@gmail.com>
2023-05-01 23:15:33 +0530

    26/4/2023

--- ZTM/Day8.py
+++ ZTM/Day8.py
@@ -1,7 +1,17 @@
 # OOP
 
-class PlayerCharacters
+class PlayerCharacter:
     def __int__(self, name):
         self.name = name
 
-    def   run
+    def shout(self):
+        print(f'my name is {self.name}')
+
+
+player1 = PlayerCharacter('Cindy', 44)
+player2 = PlayerCharacter('tom', 21)
+player2.attack = 50
+
+print(player1.shout)
+
+print(player2.name)
