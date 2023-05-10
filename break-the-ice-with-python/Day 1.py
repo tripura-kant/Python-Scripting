5ede06398794f837ab877785366875948bd40fa7
Tripura kant <tripurakant@gmail.com>
2023-05-10 22:09:27 +0530

    26/4/2023

---
+++ break-the-ice-with-python/Day 1.py
@@ -0,0 +1,16 @@
+# Fortune Cookie Program
+import random
+
+fortune = random.randint(0, 4)
+
+if fortune == 0:
+    print("May you one day be carbon neutral.")
+
+elif fortune == 1:
+    print("You have rice in your teeth.")
+
+elif fortune == 2:
+    print("A conclusion is simply the place where you got tired of thinking.")
+
+elif fortune == 3:
+    print("No snowflake feels responsible in an avalanche.")    
