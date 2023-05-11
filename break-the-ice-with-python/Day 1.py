25bf19a1abe483b4a01b9d22fb32162ad5c2b93d
Tripura kant <tripurakant@gmail.com>
2023-05-11 14:34:16 +0530

    26/4/2023

--- break-the-ice-with-python/Day 1.py
+++ break-the-ice-with-python/Day 1.py
@@ -13,6 +13,4 @@ elif fortune == 2:
     print("A conclusion is simply the place where you got tired of thinking.")
 
 elif fortune == 3:
-    print("No snowflake feels responsible in an avalanche.")
-
-
+    print("No snowflake feels responsible in an avalanch.")
