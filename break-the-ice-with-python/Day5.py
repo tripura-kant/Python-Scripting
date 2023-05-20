39598c9bf955e364614b064b55ffd8f95927b97f
Tripura kant <tripurakant@gmail.com>
2023-05-20 21:59:28 +0530

    20/5

---
+++ break-the-ice-with-python/Day5.py
@@ -0,0 +1,9 @@
+# Kilometer to miles convert
+
+Kilometer = float(input("Enter the kilometer value: "))
+
+conv_fac = 0.621371
+
+miles = Kilometer * conv_fac
+
+print('%0.2f kilometers is equal to %0.2f miles' %(Kilometer,miles))
