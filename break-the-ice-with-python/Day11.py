74814dc2e04154cc885ce5c87770b10b86be4bfc
Tripura kant <tripurakant@gmail.com>
2023-05-22 01:10:09 +0530

    werwerwe

---
+++ break-the-ice-with-python/Day11.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number: "))
+
+if number > 10:
+    print("Number is greater than 10")
+elif number < 10:
+    print("Number is less than 10")
+else:
+    print("Number is equal to 10")
