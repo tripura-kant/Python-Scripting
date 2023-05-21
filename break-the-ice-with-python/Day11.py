457b81d8c55ec2748c5b3b146ca22592ea1f99d9
Tripura kant <tripurakant@gmail.com>
2023-05-22 01:20:54 +0530

    sdf

--- break-the-ice-with-python/Day11.py
+++ break-the-ice-with-python/Day11.py
@@ -1,8 +1,24 @@
-number = float(input("Enter a number: "))
+# number = float(input("Enter a number: "))
 
-if number > 10:
-    print("Number is greater than 10")
-elif number < 10:
-    print("Number is less than 10")
+# if number > 10:
+#     print("Number is greater than 10")
+# elif number < 10:
+#     print("Number is less than 10")
+# else:
+#     print("Number is equal to 10")
+
+#Python Program to check largest number among three numbers
+
+
+num1 = float(input("Enter a num1: "))
+num2 = float(input("Enter a num2: "))
+num3 = float(input("Enter a num3: "))
+
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    largest = num2
 else:
-    print("Number is equal to 10")
+    largest = num3   
+
+print("The largest number is {0}".format(largest))
