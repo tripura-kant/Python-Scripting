f4707da11e1907a3c77134a26f4577c0bf749103
Tripura kant <tripurakant@gmail.com>
2023-06-01 10:01:58 +0530

    20/5

--- break-the-ice-with-python/Progress/Day1.py
+++ break-the-ice-with-python/Progress/Day1.py
@@ -1,29 +1,29 @@
-# Question 1-3
+Question 1-3
 
-# l = []
+l = []
 
-# for i in range(2000, 3201):
+for i in range(2000, 3201):
 
-# 	if (i%7==0) and (i%5!=0):
+	if (i%7==0) and (i%5!=0):
 
-# 		l.append(str(i))
+		l.append(str(i))
 
-# print ('.'.join(l))	
-# print(l)
-
-
-# Question 2
-
-# def fact(x):
-# 	if x == 0:
-# 		return 1
-# 	return x * fact(x - 1)
+print ('.'.join(l))	
+print(l)
 
 
-# x = int(input())
-# print(fact(x))
+Question 2
 
-# Question 3
+def fact(x):
+	if x == 0:
+		return 1
+	return x * fact(x - 1)
+
+
+x = int(input())
+print(fact(x))
+
+Question 3
 
 n = int(input())
 
