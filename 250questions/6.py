62ca3425f1738b584dfe34051d2761c815fb04c2
Tripura kant <tripurakant@gmail.com>
2023-07-02 14:43:49 +0530

    ok

--- 250questions/6.py
+++ 250questions/6.py
@@ -1,1 +1,12 @@
 # This is file 6.py
+
+def removeList():
+    l1 = [1, 4, 9, 10, 23]
+    l2 = [4, 9]
+    for elem in l2:
+        l1.remove(elem)
+    return l1
+
+
+l1 = removeList()
+print(l1)
--- 250questions/7.py
+++ 250questions/7.py
@@ -1,1 +1,11 @@
 # This is file 7.py
+# Cube of number between 1 to 21
+
+
+def getCube():
+    li = [x * x * x for x in range(1, 21)]
+    return li
+
+
+li = getCube()
+print(li)
