457872fb23504768444f452bff67b339c7e0ba49
Tripura kant <tripurakant@gmail.com>
2023-07-12 01:02:48 +0530

    ok

--- 250questions/32.py
+++ 250questions/32.py
@@ -1,1 +1,12 @@
 # This is file 32.py
+
+def totalStudents(students):
+    return (len(students.keys()))
+
+
+students = {
+    "Peter": {"age": 10, "address": "Lisbon"},
+    "Isabel": {"age": 11, "address": "Sesimbra"},
+    "Anna": {"age": 9, "address": "Lisbon"},
+}
+print(totalStudents(students))
--- 250questions/33.py
+++ 250questions/33.py
@@ -1,1 +1,15 @@
 # This is file 33.py
+
+def totalStudents(students):
+
+    for words in students:
+        print(students{})
+    return len(students)
+
+
+students = {
+    "Peter": {"age": 10, "address": "Lisbon"},
+    "Isabel": {"age": 11, "address": "Sesimbra"},
+    "Anna": {"age": 9, "address": "Lisbon"},
+}
+print(totalStudents(students))
