f4ac9bebdfe70de2e1d0bd75ce1c3cd2c52a94f0
Tripura kant <tripurakant@gmail.com>
2023-07-12 11:25:38 +0530

    ok

--- 250questions/39.py
+++ 250questions/39.py
@@ -1,1 +1,29 @@
 # This is file 39.py
+
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):  # class constructor
+        if x1 < x2 and y1 > y2:
+            self.x1 = x1  # class variable
+            self.y1 = y1  # class variable
+            self.x2 = x2  # class variable
+            self.y2 = y2  # class variable
+        else:
+            print("Incorrect coordinates of the rectangle!")
+
+    def width(self):
+        return self.x2 - self.x1
+
+    def height(self):
+        return self.y1 - self.y2
+
+    def area(self):
+        return self.width() * self.height()
+
+    def perimeter(self):
+        return 2 * self.width() + 2 * self.height()
+
+
+# test your code
+r = Rectangle(2, 7, 8, 4)
+print("Area: " + str(r.area()))
+print("Perimeter: " + str(r.perimeter()))
