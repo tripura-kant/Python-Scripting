783b2dcf2f64dd56f082cf106f70813ed9093424
Tripura kant <tripura.kant@zoom.us>
2023-09-22 00:32:52 +0530

    20/5

--- 100daysofCode-Udemy/25.py
+++ 100daysofCode-Udemy/25.py
@@ -1,1 +1,23 @@
-#
+# Milestone Project Tic toc toe
+
+
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+row1 = ['', '', '']
+row2 = ['', '', '']
+row3 = ['', '', '']
+
+row2[0] = 'X'
+display(row1, row2, row3)
+
+
+def user_choice():
+    choice = input("Enter a number (0-10): ")
+    return int(choice)
+
+
+user_choice()
