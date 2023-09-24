5e143e4f25d093700a432df3e5b40679055b2d1c
Tripura kant <tripura.kant@zoom.us>
2023-09-24 20:23:25 +0530

    20/5

--- 100daysofCode-Udemy/31.py
+++ 100daysofCode-Udemy/31.py
@@ -1,1 +1,13 @@
-# fibonacci
+# Tic-toc-toe
+
+from
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+
+
+def display_board(board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print(board[3] + '|' + board[2] + '|' + board[1])
+
+
+display_board(test_board)
