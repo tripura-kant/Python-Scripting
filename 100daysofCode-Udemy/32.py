628be18ad0970d81d4a9072dc327e920b9618848
Tripura kant <tripura.kant@zoom.us>
2023-09-28 00:12:00 +0530

    24 SEP

--- 100daysofCode-Udemy/32.py
+++ 100daysofCode-Udemy/32.py
@@ -1,8 +1,10 @@
 class Dog():
-    def __init__(self, bread):
+    def __init__(self, bread, name, spots):
         self.bread = bread
+        self.name = name
+        self.spots = spots
 
 
-my_dog = Dog(bread='lab')
+my_dog = Dog(bread='lab', name='Sammy', spots=False)
 
-print(my_dog.bread)
+print(my_dog.name)
