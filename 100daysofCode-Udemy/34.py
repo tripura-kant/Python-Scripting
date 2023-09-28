76475475b075fe7db276670851aea6309b340474
Tripura kant <tripura.kant@zoom.us>
2023-09-29 00:51:03 +0530

    29 SEP

--- 100daysofCode-Udemy/34.py
+++ 100daysofCode-Udemy/34.py
@@ -4,7 +4,14 @@ class Animal():
     def __init__(self):
         print('hiiiiii')
 
+    def who_am_i(self):
+        print("I am an animal")
+
+    def eat(self):
+        print("I am eating")
 
 myanimal = Animal()
 
+print(myanimal.eat())
 print(myanimal)
+print(myanimal.who_am_i())
