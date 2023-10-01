96c35e955a2229e2f6d7a8406b1a20edac6b2b0e
Tripura kant <tripura.kant@zoom.us>
2023-10-01 23:19:44 +0530

    29 SEP

--- 100daysofCode-Udemy/36.py
+++ 100daysofCode-Udemy/36.py
@@ -1,1 +1,14 @@
-print('Welcome 36.py')
+##
+
+class Book():
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+
+b = Book('Python rocks', 'Jane', '50')
+
+print(b)
+print(b.pages)
