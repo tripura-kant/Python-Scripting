185a6d673a127355f76a266c8602c9bf76cd0e54
Tripura kant <tripura.kant@zoom.us>
2023-10-01 23:49:57 +0530

    29 SEP

--- 100daysofCode-Udemy/37.py
+++ 100daysofCode-Udemy/37.py
@@ -1,1 +1,25 @@
-print('Welcome 37.py')
+class line:
+
+    def __init__(self, cor1, cor2):
+        self.cor1 = cor1
+        self.cor2 = cor2
+
+    def distance(self):
+        x1, y1 = self.cor1
+        x2, y2 = self.cor2
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+    def slope(self):
+        x1, y1 = self.cor1
+        x2, y2 = self.cor2
+
+        return (y2 - y1) / (x2 - x1)
+
+
+c1 = (3, 2)
+c2 = (8, 10)
+
+myLine = line(c1, c2)
+
+print(myLine.distance())
+print(myLine.slope())
--- 100daysofCode-Udemy/38.py
+++ 100daysofCode-Udemy/38.py
@@ -1,1 +1,18 @@
-print('Welcome 38.py')
+class cyclinder:
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        return self.height * 3.14 * (self.radius) ** 2
+
+    def surface_area(self):
+        top = 3.14 * (self.radius ** 2)
+
+        return (2 * top) + (2 * 3.14 * self.radius * self.height)
+
+
+mycyclinder = cyclinder(2, 3)
+
+print(mycyclinder.volume())
