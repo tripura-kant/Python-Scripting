9b806974865884aac79e532a5e6ee88fb4ab3531
Tripura kant <tripura.kant@zoom.us>
2023-10-03 00:33:31 +0530

    29 SEP

---
+++ 100daysofCode-Udemy/one.py
@@ -0,0 +1,15 @@
+# ONE.PY
+
+def func():
+    print('FUNC() IN ONE.PY')
+
+
+def func2():
+    print('FUNC() IN two.PY')
+
+
+print("TOP Level in one.py")
+
+if __name__ == '__main__':
+    func()
+    func2()
