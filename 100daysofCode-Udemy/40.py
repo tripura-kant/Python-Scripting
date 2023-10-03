3be7ac73ed9eb519323231e3ed1ba13d9e5baa4f
Tripura kant <tripura.kant@zoom.us>
2023-10-04 00:43:21 +0530

    29 SEP

--- 100daysofCode-Udemy/40.py
+++ 100daysofCode-Udemy/40.py
@@ -1,1 +1,9 @@
-print('Welcome 40.py')
+try:
+    f = open('testfile', 'r')
+    f.write("Write a test line")
+except TypeError:
+    print('There was type error')
+except OSError:
+    print('There was os error')
+finally:
+    print('I always run')
