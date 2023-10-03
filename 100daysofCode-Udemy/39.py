f87583b96bc4a3226d5c72ff0bea9163d3414cda
Tripura kant <tripura.kant@zoom.us>
2023-10-04 00:30:45 +0530

    29 SEP

--- 100daysofCode-Udemy/39.py
+++ 100daysofCode-Udemy/39.py
@@ -1,1 +1,9 @@
-print('Welcome 39.py')
+## Error Handling
+
+try:
+    result = 10 + 10
+except:
+    print("Hey it looks like you arent adding correctly")
+else:
+    print(result)
+    print('Add went well')
