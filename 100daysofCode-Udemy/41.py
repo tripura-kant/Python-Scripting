c3517e04853849eaad6fd451cebc68854b3d2b29
Tripura kant <tripura.kant@zoom.us>
2023-10-04 10:08:31 +0530

    29 SEP

--- 100daysofCode-Udemy/41.py
+++ 100daysofCode-Udemy/41.py
@@ -1,1 +1,7 @@
-print('Welcome 41.py')
+## Error and handling homework solution lecture
+
+try:
+    for i in ['a', 'b', 'c']:
+        print(i ** 2)
+except TypeError:
+    print('TypeError! Watch')
