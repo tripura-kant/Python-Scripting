f7a1e9128aada68f662a32890edfa59730d0a461
Tripura kant <tripura.kant@zoom.us>
2023-10-06 11:06:29 +0530

    Automated commit at Fri Oct  6 11:06:29 IST 2023

--- 100daysofCode-Udemy/47.py
+++ 100daysofCode-Udemy/47.py
@@ -2,4 +2,26 @@ def func():
     return 1
 
 
-print(func())
+# print(func())
+
+
+def hello(name='Jose'):
+    print('The hello() function has been executed. ')
+
+    def greet():
+        return '\t This is the greet() function inside hello!'
+
+    def welcome():
+        return '\t This is welcome() function inside hello'
+
+    if name == 'Jose':
+        return greet
+    else:
+        return welcome
+
+    # print(greet())
+    # print(welcome())
+
+
+my_new_func = hello('Jose')
+print(my_new_func())
--- 100daysofCode-Udemy/48.py
+++ 100daysofCode-Udemy/48.py
@@ -1,1 +1,10 @@
-print('Welcome 48.py')
+def cool():
+    def supercool():
+        return 'I am very cool'
+
+    return supercool
+
+
+some_func = cool()
+
+print(some_func())
