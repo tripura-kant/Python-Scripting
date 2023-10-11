5d6705a21fc9af5efc7f81e6f78aa1d9d2f2011d
Tripura kant <tripura.kant@zoom.us>
2023-10-11 15:32:33 +0530

    Automated commit at Wed Oct 11 15:32:33 IST 2023

--- 100daysofCode-Udemy/54.py
+++ 100daysofCode-Udemy/54.py
@@ -1,1 +1,12 @@
-print('Welcome 54.py')
+from datetime import datetime
+
+# my_time = datetime.time(2, 30)
+#
+# print(my_time.minute)
+# print(my_time)
+
+
+# today = datetime.date.today()
+# print(today.ctime())
+
+datetime
