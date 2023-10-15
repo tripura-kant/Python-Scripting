169b6d64a62c65e6d4b121e2e58b312a9fcb7b8f
Tripura kant <tripura.kant@zoom.us>
2023-10-15 12:54:25 +0530

    Automated commit at Sun Oct 15 12:54:25 IST 2023

--- 100daysofCode-Udemy/55.py
+++ 100daysofCode-Udemy/55.py
@@ -1,1 +1,14 @@
-print('Welcome 55.py')
+# Modules math and Random Modules
+
+import math
+from math import pi
+
+value = 4.5
+
+print(math.floor(value))
+print(math.ceil(value))
+print(round(value))
+print(pi)
+print(math.e)
+print(math.inf)
+print(math.e)
