53da10d19ac848ec187b6f22c0ff70b248ae7d13
Tripura kant <tripura.kant@zoom.us>
2024-02-07 16:43:03 +0530

    Auto commit on 2024-02-07 16:43:01

--- 100daysofCode-Udemy/62.py
+++ 100daysofCode-Udemy/62.py
@@ -1,1 +1,8 @@
 ## Edureka Python Program1
+
+'''
+
+this is python
+'''
+
+print('python')
--- 100daysofCode-Udemy/63.py
+++ 100daysofCode-Udemy/63.py
@@ -1,1 +1,13 @@
 print('Welcome 63.py')
+
+# If else
+
+a = 37
+b = 36
+
+if (a > b):
+    print("b is greater than a")
+    print("hello")
+else:
+    print("a is greater than b")
+   
