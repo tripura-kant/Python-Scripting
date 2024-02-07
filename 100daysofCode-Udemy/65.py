cf87a4cbf8cfc11c10378a441955366fd4a3cc23
Tripura kant <tripura.kant@zoom.us>
2024-02-08 02:43:02 +0530

    Auto commit on 2024-02-08 02:43:01

--- 100daysofCode-Udemy/65.py
+++ 100daysofCode-Udemy/65.py
@@ -1,4 +1,22 @@
 '''
-Write a program to find the factors of agiven number and check whether the factor is even or odd
+Write a program to find the factors of a given number and check whether the factor is even or odd
 
 '''
+
+
+def factor_and_even_odd_check(num):
+    factors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.append(i)
+            if i % 2 == 0:
+                print(f"{i} is even")
+            else:
+                print(f"{i} is odd")
+
+
+# taking input from user
+
+num = int(input("Enter a number "))
+print(f"The factors of {num} are : ")
+factor_and_even_odd_check(num)
--- 100daysofCode-Udemy/66.py
+++ 100daysofCode-Udemy/66.py
@@ -1,1 +1,18 @@
-print('Welcome 66.py')
+'''
+2.Write a code that accepts a sequence of words as input and prints the words in a
+ sequence after sorting them alphabetically.Hint: 
+Use split() to split the string into a list and then apply sort()
+'''
+
+
+def sort_alphabet(string):
+    string1 = string.split()
+    print(string1)
+    x = sorted(string1)
+    print(x)
+    print("Words sorted alphabetically:")
+    print(' '.join(x))
+
+
+string = input("Enter words ")
+sort_alphabet(string)
