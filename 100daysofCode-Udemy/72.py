4f0270fa9cfbc582e3b41254d736a972ee7b0c98
Tripura kant <tripura.kant@zoom.us>
2024-02-15 00:44:01 +0530

    Auto commit on 2024-02-15 00:43:59

---
+++ 100daysofCode-Udemy/72.py
@@ -0,0 +1,10 @@
+# Dictionary {key1: value1, key2: value2....}
+emp = {'monu': 3400, 'rahul': 3000}
+print(emp)
+# print(type(emp))
+
+for k in emp.keys():
+    print(k)
+
+for k in emp.values():
+    print(k)
--- 100daysofCode-Udemy/73.py
+++ 100daysofCode-Udemy/73.py
@@ -1,1 +1,12 @@
 print('Welcome 73.py')
+
+motorcycles = []
+
+flag = 'C'
+
+while flag == 'c' or flag == 'C':
+    name = input("Enter a motorcycle name :: ")
+    motorcycles.append(name)
+    flag = input("Enter C: continue, P: printing the list, any other to break: ")
+    if flag == 'P' or flag == 'p':
+        print("The name of motor cycles are: ", motorcycles)
