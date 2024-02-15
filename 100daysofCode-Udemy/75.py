ecf118598f96be496af7c44d8f5116768ed940b4
Tripura kant <tripura.kant@zoom.us>
2024-02-15 12:43:27 +0530

    Auto commit on 2024-02-15 12:43:26

--- 100daysofCode-Udemy/75.py
+++ 100daysofCode-Udemy/75.py
@@ -1,1 +1,14 @@
-print('Welcome 75.py')
+import os
+
+file_path = '/Users/tripurakant/Documents/Imp-folder/py/Python-Scripting/100daysofCode-Udemy/File_new.txt'
+
+try:
+    with open(file_path, mode='w') as myfile:
+        for i in range(1, 5, 3):
+            myfile.write("The file is opened in write mode")
+
+    print("File created successfully at:", file_path)
+except Exception as e:
+    print("An error occurred:", str(e))
+    # Remove the file if an error occurs
+    os.remove(file_path)
