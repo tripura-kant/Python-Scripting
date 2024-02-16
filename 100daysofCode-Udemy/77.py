f036caca803941cbca976a465b566181e7cfaa15
Tripura kant <tripura.kant@zoom.us>
2024-02-17 00:43:06 +0530

    Auto commit on 2024-02-17 00:43:01

--- 100daysofCode-Udemy/77.py
+++ 100daysofCode-Udemy/77.py
@@ -1,1 +1,9 @@
-print('Welcome 77.py')
+file_obj = open('File2.txt', 'r')
+print(file_obj.tell())
+file_obj.read()
+print(file_obj.tell())
+output = file_obj.read()
+while True:
+    for i in output:
+        print(output)
+    break
