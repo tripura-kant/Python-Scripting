5a6c084e491675bc590cffe58839c4a1d042f691
Tripura kant <tripura.kant@zoom.us>
2024-05-12 23:49:01 +0530

    new

--- Python-Bootcamp/Day2.py
+++ Python-Bootcamp/Day2.py
@@ -11,38 +11,56 @@
 #
 # print(123 + 345)
 
-num_char = len(input("What is your name? "))
+# num_char = len(input("What is your name? "))
+#
+# new_num_char = str(num_char)
+#
+# print("Your name has " + new_num_char + " characters.")
+#
+#
+# two_digit_number = input()
+# # 🚨 Don't change the code above 👆
+# ####################################
+# # Write your code below this line 👇
+#
+# num1=int(two_digit_number[0])
+# num2=int(two_digit_number[1])
+#
+# print(num1 + num2)
+#
+#
+# # BMI calculator
+#
+# # 1st input: enter height in meters e.g: 1.65
+# height = input()
+# # 2nd input: enter weight in kilograms e.g: 72
+# weight = input()
+# # 🚨 Don't change the code above 👆
+#
+# # Write your code below this line 👇
+#
+# height_float = float(height)
+#
+# weight_int = int(weight)
+#
+# bmi = weight_int / height_float ** 2
+#
+# print(int(bmi))
 
-new_num_char = str(num_char)
 
-print("Your name has " + new_num_char + " characters.")
+score = 3
+height = 1.8
+isWinning = True
+score += 1
 
+print(f" your score is {height}, you are wiing by {score}")
 
-two_digit_number = input()
+age = int(input("age"))
 # 🚨 Don't change the code above 👆
-####################################
 # Write your code below this line 👇
 
-num1=int(two_digit_number[0])
-num2=int(two_digit_number[1])
+years_left = 90 - age
 
-print(num1 + num2)
+week_left = 52 * years_left
 
-
-# BMI calculator
-
-# 1st input: enter height in meters e.g: 1.65
-height = input()
-# 2nd input: enter weight in kilograms e.g: 72
-weight = input()
-# 🚨 Don't change the code above 👆
-
-# Write your code below this line 👇
-
-height_float = float(height)
-
-weight_int = int(weight)
-
-bmi = weight_int / height_float ** 2
-
-print(int(bmi))
+print(f"You have {week_left} weeks left.")
