00c9c78eee787bf48ddd963653c90b020205ecce
Tripura kant <tripura.kant@zoom.us>
2024-05-14 01:22:19 +0530

    new

--- Python-Bootcamp/Day3.py
+++ Python-Bootcamp/Day3.py
@@ -1,7 +1,7 @@
 print("Welcome to roalacoaster")
 height = int(input("What is your height in cm "))
 
-if height > 120:
+if height >= 120:
     print("You can ride the roalacoaster")
 else:
     print("You cannot ride")
