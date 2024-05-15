cab4395ef04fc1a02f4076fde30a91c52247323d
Tripura kant <tripura.kant@zoom.us>
2024-05-15 21:54:22 +0530

    new

--- Python-Bootcamp/Day3.py
+++ Python-Bootcamp/Day3.py
@@ -33,4 +33,8 @@ if height >=120:
         bill = 12
         print("Your ticket is $12 ")
 
-    wants_photo = input()
+    wants_photo = input("Do you want a photo taken ? Y OR N")
+        if wants_photo == "Y":
+            bill += 3
+
+    print(f"Your final bill is {bill}")
