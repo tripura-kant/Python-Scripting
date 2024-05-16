cf9924b7bcd0dbb04498dd97ff56b1b12edd5a68
Tripura kant <tripura.kant@zoom.us>
2024-05-16 11:18:01 +0530

    new

--- Python-Bootcamp/Day3.py
+++ Python-Bootcamp/Day3.py
@@ -42,26 +42,63 @@
 #     print("Sorry you need to grow older")
 
 # print("Thank you for choosing Python Pizza Deliveries!")
-size = input()  # What size pizza do you want? S, M, or L
+# size = input()  # What size pizza do you want? S, M, or L
+#
+# if size == "L":
+#     bill = 25
+#     add_pepperoni = input()  # Do you want pepperoni? Y or N
+#     if add_pepperoni == "Y":
+#         bill += 3
+# elif size == "M":
+#     bill = 20
+#     add_pepperoni = input()  # Do you want pepperoni? Y or N
+#     if add_pepperoni == "Y":
+#         bill += 3
+# elif size == "S":
+#     bill = 15
+#     add_pepperoni = input()  # Do you want pepperoni? Y or N
+#     if add_pepperoni == "Y":
+#         bill += 2
+#
+# add_chesse = input()
+# if add_chesse == "Y":
+#     bill += 1
+#
+# print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
 
-if size == "L":
-    bill = 25
-    add_pepperoni = input()  # Do you want pepperoni? Y or N
-    if add_pepperoni == "Y":
-        bill += 3
-elif size == "M":
-    bill = 20
-    add_pepperoni = input()  # Do you want pepperoni? Y or N
-    if add_pepperoni == "Y":
-        bill += 3
-elif size == "S":
-    bill = 15
-    add_pepperoni = input()  # Do you want pepperoni? Y or N
-    if add_pepperoni == "Y":
-        bill += 2
+print("The Love Calculator is calculating your score...")
+name1 = input() # What is your name?
+name2 = input() # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+combine = name1 + name2
 
-add_chesse = input()
-if add_chesse == "Y":
-    bill += 1
+lowercase = combine.lower()
 
-print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+t = lowercase.count("t")
+r = lowercase.count("r")
+u = lowercase.count("u")
+e = lowercase.count("e")
+
+first_digit = t + r + u + e
+
+
+l = lowercase.count("l")
+o = lowercase.count("o")
+v = lowercase.count("v")
+e = lowercase.count("e")
+second_digit = l + o + v + e
+
+
+
+score = int(str(first_digit) + str(second_digit))
+
+
+
+if score < 10 or score > 90:
+  print(f"Your score is {score}, you go together like coke and mentos.")
+
+elif 40 <= score <= 50:
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
