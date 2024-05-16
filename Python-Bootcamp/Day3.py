4de4a0e021250a5dab8bed1eab5001c6b145bff7
Tripura kant <tripura.kant@zoom.us>
2024-05-17 01:00:36 +0530

    new

--- Python-Bootcamp/Day3.py
+++ Python-Bootcamp/Day3.py
@@ -65,40 +65,45 @@
 #     bill += 1
 #
 # print(f"Thank you for choosing Python Pizza Deliveries! \nYour final bill is: ${bill}.")
+#
+# print("The Love Calculator is calculating your score...")
+# name1 = input() # What is your name?
+# name2 = input() # What is their name?
+# # 🚨 Don't change the code above 👆
+# # Write your code below this line 👇
+# combine = name1 + name2
+#
+# lowercase = combine.lower()
+#
+# t = lowercase.count("t")
+# r = lowercase.count("r")
+# u = lowercase.count("u")
+# e = lowercase.count("e")
+#
+# first_digit = t + r + u + e
+#
+#
+# l = lowercase.count("l")
+# o = lowercase.count("o")
+# v = lowercase.count("v")
+# e = lowercase.count("e")
+# second_digit = l + o + v + e
+#
+#
+#
+# score = int(str(first_digit) + str(second_digit))
+#
+#
+#
+# if score < 10 or score > 90:
+#   print(f"Your score is {score}, you go together like coke and mentos.")
+#
+# elif 40 <= score <= 50:
+#   print(f"Your score is {score}, you are alright together.")
+# else:
+#   print(f"Your score is {score}.")
 
-print("The Love Calculator is calculating your score...")
-name1 = input() # What is your name?
-name2 = input() # What is their name?
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-combine = name1 + name2
+import random
 
-lowercase = combine.lower()
-
-t = lowercase.count("t")
-r = lowercase.count("r")
-u = lowercase.count("u")
-e = lowercase.count("e")
-
-first_digit = t + r + u + e
-
-
-l = lowercase.count("l")
-o = lowercase.count("o")
-v = lowercase.count("v")
-e = lowercase.count("e")
-second_digit = l + o + v + e
-
-
-
-score = int(str(first_digit) + str(second_digit))
-
-
-
-if score < 10 or score > 90:
-  print(f"Your score is {score}, you go together like coke and mentos.")
-
-elif 40 <= score <= 50:
-  print(f"Your score is {score}, you are alright together.")
-else:
-  print(f"Your score is {score}.")
+random_integer = random.randint(1, 10)
+print(random_integer)
