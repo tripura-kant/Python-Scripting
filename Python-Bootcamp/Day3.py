3defa76bf37ec72689914c72b482ac1f55d6170b
Tripura kant <tripura.kant@zoom.us>
2024-05-17 10:06:06 +0530

    new

--- Python-Bootcamp/Day3.py
+++ Python-Bootcamp/Day3.py
@@ -107,25 +107,33 @@
 #
 # random_integer = random.randint(1, 10)
 # print(random_integer)
+#
+# import random
+# random_num = random.randint(0, 1)
+# #print(random_num)
+#
+# if random_num == 1:
+#   print("Heads")
+# if random_num == 0:
+#   print("Tails")
+#
+# names = names_string.split(", ")
+# # names_string contains the input values provided.
+# # The code above converts the input into an array seperating
+# # each name in the input by a comma and space.
+# # 🚨 Don't change the code above 👆
+# import random
+#
+# num_items = len(names)
+#
+# random_choice = random.randint(0, num_items-1)
+#
+# print(f"{names[random_choice]} is going to buy the meal today!")
 
-import random
-random_num = random.randint(0, 1)
-#print(random_num)
 
-if random_num == 1:
-  print("Heads")
-if random_num == 0:
-  print("Tails")
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 
-names = names_string.split(", ")
-# names_string contains the input values provided.
-# The code above converts the input into an array seperating
-# each name in the input by a comma and space.
-# 🚨 Don't change the code above 👆
-import random
+dirty_dozen = [fruits, vegetables]
 
-num_items = len(names)
-
-random_choice = random.randint(0, num_items-1)
-
-print(f"{names[random_choice]} is going to buy the meal today!")
+print(dirty_dozen)
