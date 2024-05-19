0a2297f672db089209aa218ddef0f4fd695d679a
Tripura kant <tripura.kant@zoom.us>
2024-05-19 14:58:39 +0530

    new

--- Python-Bootcamp/Day5.py
+++ Python-Bootcamp/Day5.py
@@ -1,3 +1,22 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+
+
+# Input a Python list of student heights
+student_heights = input("enter student").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+total_heights = 0
+
+for height in student_heights:
+    total_heights += height
+
+no_st = len(student_heights)
+
+print(f"total height = {total_heights}")
+print(f"number of students = {no_st}")
+print(f"average height = {total_heights}")
