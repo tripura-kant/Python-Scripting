5bb0d13764e68dae3b34553709050acbf46a7b48
Tripura kant <tripura.kant@zoom.us>
2024-05-19 18:12:38 +0530

    new

--- Python-Bootcamp/Day5.py
+++ Python-Bootcamp/Day5.py
@@ -24,19 +24,20 @@
 # print(f"average height = {avg_heights}")
 
 # for loop to find highest score
+#
+#     # # Input a list of student scores
+# student_scores = input("Enter scores    ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+#
+# # Write your code below this row 👇
+# print(student_scores)
+#
+# heighest_score = 0
+#
+# for score in student_scores:
+#     if score > heighest_score:
+#         heighest_score = score
+#
+# print(f"The highest score in the class is: {heighest_score}")
 
-    # # Input a list of student scores
-student_scores = input("Enter scores    ").split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-
-# Write your code below this row 👇
-print(student_scores)
-
-heighest_score = 0
-
-for score in student_scores:
-    if score > heighest_score:
-        heighest_score = score
-
-print(f"The highest score in the class is: {heighest_score}")
