58b9bbd5e479309c1409059872ab5172fda7ce4d
Tripura kant <tripura.kant@zoom.us>
2024-05-22 01:36:53 +0530

    new

--- Python-Bootcamp/Day6.py
+++ Python-Bootcamp/Day6.py
@@ -17,20 +17,42 @@ def turn_right():
     turn_left()
 
 
-def jump():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
 
 
-jump()
-jump()
-jump()
-jump()
-jump()
-jump()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# for step in range(6):
+#     jump()
+
