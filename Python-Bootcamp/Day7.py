d98d0f2369553b7d831cdc1f186487b4049f3fdd
Tripura kant <tripura.kant@zoom.us>
2024-05-25 00:12:26 +0530

    new

--- Python-Bootcamp/Day7.py
+++ Python-Bootcamp/Day7.py
@@ -2,6 +2,10 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+
 guess = input("Guess a letter: ").lower()
 for letter in chosen_word:
   if letter == guess:
