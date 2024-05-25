167b8e616eff9630772e65059043f6ea6837d91b
Tripura kant <tripura.kant@zoom.us>
2024-05-25 20:12:01 +0530

    new

--- Python-Bootcamp/Day8.py
+++ Python-Bootcamp/Day8.py
@@ -24,19 +24,35 @@
 
 
 # Write your code below this line 👇
-import math
+# import math
+#
+#
+# def paint_calc(height, width, cover):
+#     cans = (height * width) / cover
+#     print(f"You'll need {math.ceil(cans)} cans of paint.")
+#
+#
+# # Write your code above this line 👆
+# # Define a function called paint_calc() so the code below works.
+#
+# # 🚨 Don't change the code below 👇
+# test_h = int(input("height  "))  # Height of wall (m)
+# test_w = int(input("width   "))  # Width of wall (m)
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
 
 
-def paint_calc(height, width, cover):
-    cans = (height * width) / cover
-    print(f"You'll need {math.ceil(cans)} cans of paint.")
+## Prime checker
 
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number")
+    else:
+        print("it's not a prime number")
 
-# Write your code above this line 👆
-# Define a function called paint_calc() so the code below works.
-
-# 🚨 Don't change the code below 👇
-test_h = int(input("height  "))  # Height of wall (m)
-test_w = int(input("width   "))  # Width of wall (m)
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+n = input("Enter num")
+prime_checker(number=n)
