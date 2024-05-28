beb69f60037dc91089cbe09cca830b8555524828
Tripura kant <tripura.kant@zoom.us>
2024-05-28 23:42:32 +0530

    new

--- Python-Bootcamp/Day9.py
+++ Python-Bootcamp/Day9.py
@@ -1,6 +1,8 @@
 # Dictionary
 
 programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running"
-    "Fuction": ""
+    "Bug": "An error in a program that prevents the program from running",
+    "Fuction": "A piece of cpde"
 }
+
+print(programming_dictionary["Bug"])
