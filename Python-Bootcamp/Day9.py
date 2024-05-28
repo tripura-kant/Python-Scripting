d0f6f92aefdc7965374ea39001fae7d3167d0b2b
Tripura kant <tripura.kant@zoom.us>
2024-05-29 00:02:32 +0530

    new

--- Python-Bootcamp/Day9.py
+++ Python-Bootcamp/Day9.py
@@ -1,12 +1,38 @@
 # Dictionary
 
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running",
-    "Fuction": "A piece of code"
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running",
+#     "Fuction": "A piece of code"
+# }
+#
+# # print(programming_dictionary["Fuction"])
+#
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99,
+  "Draco": 74,
+  "Neville": 62,
 }
+# 🚨 Don't change the code above 👆
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
 
-# print(programming_dictionary["Fuction"])
-
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student] = "Outstanding"
+  elif score > 80:
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70:
+    student_grades[student] = "Acceptable"
+  else:
+    student_grades[student] = "Fail"
+# 🚨 Don't change the code below 👇
+print(student_grades)
