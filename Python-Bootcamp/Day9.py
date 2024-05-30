6f821d889ea6a8629a2f1d2b321ba08ab88cfbff
Tripura kant <tripura.kant@zoom.us>
2024-05-31 00:10:56 +0530

    new

--- Python-Bootcamp/Day9.py
+++ Python-Bootcamp/Day9.py
@@ -38,4 +38,33 @@
 # print(student_grades)
 
 
+country = input("enter country")  # Add country name
+visits = int(input("enter visit"))  # Number of visits
+list_of_cities = eval(input("enter list_of_cities"))  # create list from formatted string
 
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+
+
+# Do NOT change the code above 👆
+
+# TODO: Write the function that will allow new countries
+# to be added to the travel_log.
+def add_new_country(country, visits, list_of_cities):
+
+
+
+# Do not change the code below 👇
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favourite city was {travel_log[2]['cities'][0]}.")
