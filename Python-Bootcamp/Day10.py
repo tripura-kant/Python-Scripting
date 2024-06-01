94e0142ac96db668425ccd6907f1445034a1cc44
Tripura kant <tripura.kant@zoom.us>
2024-06-01 18:48:36 +0530

    new

--- Python-Bootcamp/Day10.py
+++ Python-Bootcamp/Day10.py
@@ -1,10 +1,10 @@
 # Functions with input
 
-# def format_name(f_name, l_name):
-#     print(f_name.title())
-#     print(l_name.title())
-#
-# format_name("monu", "ttk")
+def format_name(f_name, l_name):
+    print(f_name.title())
+    print(l_name.title())
+
+format_name("monu", "ttk")
 
 # More functions
 
