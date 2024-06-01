d970e77667af7a510898c82de29c8e9159f92f4b
Tripura kant <tripura.kant@zoom.us>
2024-06-01 18:54:36 +0530

    new

--- Python-Bootcamp/Day10.py
+++ Python-Bootcamp/Day10.py
@@ -1,12 +1,36 @@
 # Functions with input
 
-def format_name(f_name, l_name):
-    if f_name == "" or l_name == "":
-        return "You didn't provide valid input"
-    print(f_name.title())
-    print(l_name.title())
+# def format_name(f_name, l_name):
+#     if f_name == "" or l_name == "":
+#         return "You didn't provide valid input"
+#     print(f_name.title())
+#     print(l_name.title())
+#
+# #format_name("monu", "ttk")
+# format_name(" "," ")
+# # More functions
 
-#format_name("monu", "ttk")
-format_name(" "," ")
-# More functions
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year")
+            else:
+                print("Not leap year")
+        else:
+            print("Leap year")
+    else:
+        print("Not leap year")
+
+
+# TODO: Add more code here 👇
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+
+# 🚨 Do NOT change any of the code below
+year = int(input("enter year"))  # Enter a year
+month = int(input("enter month"))  # Enter a month
+days = days_in_month(year, month)
+print(days)
 
