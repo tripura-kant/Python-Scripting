5384ba7a4f303963b0f8a71923529a41469d6820
Tripura kant <tripura.kant@zoom.us>
2024-06-01 18:50:36 +0530

    new

--- Python-Bootcamp/Day10.py
+++ Python-Bootcamp/Day10.py
@@ -1,10 +1,12 @@
 # Functions with input
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid input"
     print(f_name.title())
     print(l_name.title())
 
-format_name("monu", "ttk")
-
+#format_name("monu", "ttk")
+format_name()
 # More functions
 
