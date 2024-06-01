ab41cf640eb0b955eb409d454bd5ba054203195b
Tripura kant <tripura.kant@zoom.us>
2024-06-01 19:24:36 +0530

    new

--- Python-Bootcamp/Day10.py
+++ Python-Bootcamp/Day10.py
@@ -10,27 +10,57 @@
 # format_name(" "," ")
 # # More functions
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                print("Leap year")
-            else:
-                print("Not leap year")
-        else:
-            print("Leap year")
-    else:
-        print("Not leap year")
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 print("Leap year")
+#             else:
+#                 print("Not leap year")
+#         else:
+#             print("Leap year")
+#     else:
+#         print("Not leap year")
+#
+#
+# # TODO: Add more code here 👇
+# def days_in_month(year, month):
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#
+#
+# # 🚨 Do NOT change any of the code below
+# year = int(input("enter year"))  # Enter a year
+# month = int(input("enter month"))  # Enter a month
+# days = days_in_month(year, month)
+# print(days)
 
+#Calculator
 
-# TODO: Add more code here 👇
-def days_in_month(year, month):
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#aDD
+def add(n1, n2):
+    return n1 + n2
 
+#Sub
+def sub(n1, n2):
+    return n1 - n2
 
-# 🚨 Do NOT change any of the code below
-year = int(input("enter year"))  # Enter a year
-month = int(input("enter month"))  # Enter a month
-days = days_in_month(year, month)
-print(days)
+#MUL
+def mul(n1, n2):
+    return n1 * n2
 
+#DIV
+def div(n1, n2):
+    return n1 / n2
+
+operation = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div
+}
+
+num1 = int(input("what is first number: "))
+num2 = int(input("what is sec number: "))
+
+for choosen_sign in operation:
+    print(choosen_sign)
