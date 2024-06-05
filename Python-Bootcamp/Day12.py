838058ea562b4789b48688f77b54377713ee0e29
Tripura kant <tripura.kant@zoom.us>
2024-06-06 00:36:40 +0530

    new

--- Python-Bootcamp/Day12.py
+++ Python-Bootcamp/Day12.py
@@ -3,6 +3,7 @@
 from random import randint
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
+guess = intinput(("Enter a guess "))
 #Set difficulty
 def set_difficulty():
     level = input("Set a level of difficulty: easy OR hard  ")
