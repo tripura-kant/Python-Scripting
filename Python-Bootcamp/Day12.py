ed00b3e029570d780317ebab825371510976ec7b
Tripura kant <tripura.kant@zoom.us>
2024-06-05 22:34:41 +0530

    new

--- Python-Bootcamp/Day12.py
+++ Python-Bootcamp/Day12.py
@@ -1,8 +1,12 @@
 # Make function to set difficulty
-
+#Set difficulty
 def set_difficulty():
     level = input("Set a level of difficulty: easy OR hard  ")
     print(f"You had selected {level}")
 
 
 set_difficulty()
+
+# choosing a random number between 1 and 100
+
+# 
