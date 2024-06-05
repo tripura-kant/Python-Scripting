8dc1297a039daaa8d7dbfb08222ad43dfbf88afd
Tripura kant <tripura.kant@zoom.us>
2024-06-06 01:18:40 +0530

    new

--- Python-Bootcamp/Day13.py
+++ Python-Bootcamp/Day13.py
@@ -10,7 +10,7 @@
 # Reproduce the Bug
 from random import randint
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 7)
+dice_num = randint(0, 5)
 print(dice_imgs[dice_num])
 
 # # Play Computer
