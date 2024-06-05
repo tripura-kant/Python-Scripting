85e176675ce37d5809658e3dcbc92f504dc2612c
Tripura kant <tripura.kant@zoom.us>
2024-06-06 00:30:40 +0530

    new

--- Python-Bootcamp/Day12.py
+++ Python-Bootcamp/Day12.py
@@ -1,10 +1,12 @@
 # Make function to set difficulty
+
+from random import randint
 #Set difficulty
 def set_difficulty():
     level = input("Set a level of difficulty: easy OR hard  ")
     print(f"You had selected {level}")
 
-
+answer = randint(1, 100)
 set_difficulty()
 
 # choosing a random number between 1 and 100
