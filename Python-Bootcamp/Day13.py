712014753ca1f60ef4bb0c7ed2478b93d642bca2
Tripura kant <tripura.kant@zoom.us>
2024-06-06 01:38:40 +0530

    new

--- Python-Bootcamp/Day13.py
+++ Python-Bootcamp/Day13.py
@@ -26,12 +26,12 @@
 #   print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page == int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
 
 # #Use a Debugger
 # def mutate(a_list):
