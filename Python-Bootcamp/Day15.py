289795494e7bca2d42c2827d9dfbbabd37f6647c
Tripura kant <tripura.kant@zoom.us>
2024-06-06 22:18:58 +0530

    new

--- Python-Bootcamp/Day15.py
+++ Python-Bootcamp/Day15.py
@@ -1,11 +1,17 @@
 # Coffee machine project
 import sys
-answer = input("What would you like? (espresso/latte/cappuccino):    ").lower
 
+water = 100
+milk = 50
+coffee = 76
+money = 2.5
 
+answer = input("What would you like? (espresso/latte/cappuccino):    ").lower()
 
 if answer == "off":
     print("Turning off the machine...!!")
     sys.exit();
 
 print(f" You choosed {answer}")
+
+if answer == "report":
