067cd0f696bc7fbd5ed292279153e252b9b197c4
Tripura kant <tripura.kant@zoom.us>
2024-06-06 22:12:58 +0530

    new

--- Python-Bootcamp/Day15.py
+++ Python-Bootcamp/Day15.py
@@ -1,6 +1,7 @@
 # Coffee machine project
-machine_state = on
-answer = input("What would you like? (espresso/latte/cappuccino).lower:    ")
+
+answer = input("What would you like? (espresso/latte/cappuccino):    ").lower
 
 if answer == "off":
-    machine_state = turn_off
+    print("Turning off the machine...!!")
+    break;
