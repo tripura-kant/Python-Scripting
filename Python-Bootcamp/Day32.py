fd13656f9e77f52e9e30efde4e66f3c794281db8
Tripura kant <tripura.kant@zoom.us>
2024-06-09 14:52:44 +0530

    new

--- Python-Bootcamp/Day32.py
+++ Python-Bootcamp/Day32.py
@@ -7,3 +7,5 @@ connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 
 connection.login(user=my_email , password=password)
+connection.sendmail(from_addr=my_email, to_addrs="tripurakant@yahoo.com", msg="hello tripura 9 jun ")
+connection.close()
