626fa79e5a61f908b9de1fd1c5f6ff6c2afd68ce
Tripura kant <tripura.kant@zoom.us>
2024-06-09 14:40:43 +0530

    new

--- Python-Bootcamp/Day32.py
+++ Python-Bootcamp/Day32.py
@@ -1,3 +1,8 @@
 import smtplib
 
-connection = smtplib.SMTP()
+my_email = "tripurakanttest@gmail.com"
+
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+
+connection.login(user= , password=)
