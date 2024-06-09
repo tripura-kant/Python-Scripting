652424d1ef7a7f86ac710eab673ec9e6e3dee0ce
Tripura kant <tripura.kant@zoom.us>
2024-06-09 14:20:43 +0530

    new

--- Python-Bootcamp/Day17.py
+++ Python-Bootcamp/Day17.py
@@ -9,11 +9,11 @@
 #
 # print(user1.name)
 
-
 class User:
-
   def __init__(self, user_id, username):
     self.id = user_id
     self.username = username
 
-    
+  User1 = User("001", "tripura")
+
+  print(User1.username)
