c694b2ad47d57349707e18ba769478321ecfd3f7
Tripura kant <tripura.kant@zoom.us>
2024-06-09 14:18:43 +0530

    new

--- Python-Bootcamp/Day17.py
+++ Python-Bootcamp/Day17.py
@@ -1,10 +1,19 @@
 # creating class
 
+# class User:
+#   pass
+#
+# user1 = User()
+# user1.id = "111"
+# user1.name = "monu"
+#
+# print(user1.name)
+
+
 class User:
-  pass
 
-user1 = User()
-user1.id = "111"
-user1.name = "monu"
+  def __init__(self, user_id, username):
+    self.id = user_id
+    self.username = username
 
-print(user1.name)
+    
