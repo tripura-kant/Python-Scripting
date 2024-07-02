92b93a15bbc8536f2091daf426ad94bfbd131efc
Tripura kant <tripura.kant@zoom.us>
2024-07-02 15:17:51 +0530

    new

---
+++ Python-Bootcamp/a24/printing-var.py
@@ -0,0 +1,11 @@
+# Printing variable
+name = "Tripura"
+
+#M-1
+print("my name is", name)
+
+#M-2
+print("My name is %s" %name)
+
+#m-3
+print(f"my name is {name}")
---
+++ Python-Bootcamp/a24/type_casting.py
@@ -0,0 +1,25 @@
+# a = "200"
+# b = "300"
+# total = a + b
+# print(total)
+#
+#
+# # type casting
+#
+# a = "200"
+# b = "300"
+# total = int(a) + int(b)
+# print(total)
+#
+# print(a, type(a))
+# print(b, type(b))
+
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+num3 = int(input("Enter num3"))
+num4 = int(input("Enter num4"))
+num5 = int(input("Enter num5"))
+
+average = ((num1 + num2 + num3 + num4 + num5) / 5)
+
+print(f" the average is {average}")
