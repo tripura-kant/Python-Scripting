519dddfdbeedcb3843a5817ed7311ada06e34920
Tripura kant <tripura.kant@zoom.us>
2024-07-03 11:48:57 +0530

    new 2024-07-03

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
+echo "Monu405#2" | sudo -S scutil --set HostName MacBook-Pro
 cd /Users/tripurakant/Documents/code/My\ git/Python-Scripting && git add . && git commit -m "new $(date +'%Y-%m-%d')" && git push
 
