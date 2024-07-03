a461ee38cd3241c2c93dc64d7c0d33fe79e205f3
Tripura kant <tripura.kant@zoom.us>
2024-07-04 00:27:00 +0530

    new

---
+++ a24/function_return.py
@@ -0,0 +1,16 @@
+def add(n1, n2):
+    total = n1 + n2
+    return total
+
+
+def even_odd(num):
+    if num % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+
+x = add(1, 4)
+print(x)
+
+even_odd(x)
---
+++ a24/function_return2.py
@@ -0,0 +1,7 @@
+def average(n1: int, n2: int) -> float:
+    total = n1 + n2
+    return total / 4
+
+
+x = average(1, 3)
+print(x)
