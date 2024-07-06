ad276f218bc7b740ef9e073ea9516802f70d9eda
Tripura kant <tripura.kant@zoom.us>
2024-07-06 19:58:10 +0530

    new 2024-07-06

---
+++ a24/ask_numbers.py
@@ -0,0 +1,8 @@
+total = 0
+while True:
+    num = int(input("Enter num  "))
+    if num == 0:
+        break
+    total = total + num
+
+print(total)
---
+++ a24/control_statements.py
@@ -0,0 +1,16 @@
+'''
+Control statements
+
+continue
+break
+
+Hello
+Hello
+
+'''
+i = 1
+while i <= 5:
+    print("Hello", end=" ")
+    if i == 3:
+        continue
+    i += 1
