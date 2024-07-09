9da477e18bf49fc0588b58d1f52fa6ba2e9a3603
Tripura kant <tripura.kant@zoom.us>
2024-07-09 17:58:00 +0530

    new 2024-07-09

--- a24/nestedloop.py
+++ a24/nestedloop.py
@@ -45,7 +45,7 @@
 #     print()
 
 n = int(input("Enter num    "))
-for i in range(1, n):
-    for j in range(1, 6):
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
         print(1, end=" ")
     print()
