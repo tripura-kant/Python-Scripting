90c9ef6f66a5fda66c5285b5b0d2380e5a72d543
Tripura kant <tripura.kant@zoom.us>
2024-07-09 12:36:24 +0530

    new 2024-07-09

---
+++ a24/nestedloop.py
@@ -0,0 +1,12 @@
+# for i in range(1, 4):
+#     print(i)
+#     for j in range(10, 13):
+#         print(j)
+
+# for _ in range(5):
+#     print("* * * * *")
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print("*", end=" ")
+    print()
