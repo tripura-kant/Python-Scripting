3fb72ade6a206f766a200ca56d71c0e025f948ad
Tripura kant <tripura.kant@zoom.us>
2024-07-12 23:58:26 +0530

    new 2024-07-12

--- a24/pattern/assignment_patter.py
+++ a24/pattern/assignment_patter.py
@@ -1,6 +1,30 @@
-n = 4
+# n = 4
+# i = 1
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+
+
+# n = 4
+# i = 1
+# for i in range(1, n + 1):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+
+# # Q3
+# n = 5
+# i = 1
+# for i in range(n, 0, -1):
+#     for j in range(i, 6):
+#         print(j, end=" ")
+#     print()
+
+# Q4
+n = 5
 i = 1
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
+for i in range(n, 0, -1):
+    for j in range(6, i, -1):
         print(j, end=" ")
     print()
