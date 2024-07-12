43cba4398414a8f51e34af78113c8873776a3fa7
Tripura kant <tripura.kant@zoom.us>
2024-07-13 00:58:26 +0530

    new 2024-07-13

--- a24/pattern/assignment_patter.py
+++ a24/pattern/assignment_patter.py
@@ -21,10 +21,53 @@
 #         print(j, end=" ")
 #     print()
 
-# Q4
-n = 5
-i = 1
-for i in range(n, 0, -1):
-    for j in range(6, i, -1):
-        print(j, end=" ")
-    print()
+# # Q4
+# n = 5
+# for i in range(n, 0, -1):
+#     for j in range(5, i - 1, -1):
+#         print(j, end=" ")
+#     print()
+
+# # Q5
+# n = 5
+# for i in range(1, n + 1):
+#     for j in range(5, i - 1, -1):
+#         print(j, end=" ")
+#     print()
+
+
+# # Q6
+# n = 6
+# for i in range(1, n + 1):
+#     for j in range(i, 6):
+#         print(j, end=" ")
+#     print()
+
+# # Q7
+# n = 5
+# for i in range(n, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end=" ")
+#     print()
+
+# # Q8
+# n = 5
+# num = 1
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print(num, end=" ")
+#         num += 1
+#     print()
+
+# 9
+def pattern(n: int):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            if j % 2 == 0:
+                print(2, end=" ")
+            else:
+                print(1, end=" ")
+        print()
+
+
+pattern(5)
