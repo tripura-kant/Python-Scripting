690d52e7b2dc0d7c7a517dfd23dcc5efc97ede7d
Tripura kant <tripura.kant@zoom.us>
2024-07-12 16:58:12 +0530

    new 2024-07-12

--- a24/qqq.py
+++ a24/qqq.py
@@ -20,12 +20,50 @@
 #
 # pattern(2)
 
-def pattern(n):
-    i = 1
-    while i < n:
-        result = i ** 2
-        print(result)
-        i += 1
+# def pattern(n):
+#     i = 0
+#     while i < n:
+#         result = 2 ** i
+#         print(result)
+#         i += 1
+#
+#
+# pattern(7)
+
+# def pattern(n: int) -> None:
+#     num = 1
+#     i = 1
+#     while i <= n:
+#         print(num)
+#         num = (num * 10) + 1
+#         i += 1
+#
+#
+# pattern(5)
+
+# def pattern(n: int):
+#     for i in range(1, n + 1):
+#         for j in range(1, 6):
+#             print(1, end=" ")
+#         print()
+#
+#
+# # Alternate Way
+# def pattern_2(n: int):
+#     for _ in range(n):
+#         for _ in range(5):
+#             print(1, end=" ")
+#         print()
+#
+#
+# pattern(3)
+# # pattern_2(3)
+
+def pattern(n: int):
+    for i in range(1, n + 1):
+        for j in range(5, 0, -1):
+            print(j, end=" ")
+        print()
 
 
-pattern(7)
+pattern(5)
