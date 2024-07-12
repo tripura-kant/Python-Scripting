53cff7ad244bb3aa0e7af0f2ca2ec9682b412361
Tripura kant <tripura.kant@zoom.us>
2024-07-12 15:58:12 +0530

    new 2024-07-12

---
+++ a24/qqq.py
@@ -0,0 +1,31 @@
+# n = int(input("enter num    "))
+
+
+#
+#
+# def total_cups(n):
+#     result = n // 6
+#     final = n + result
+#     print(int(final))
+#
+#
+# total_cups(6)
+# def pattern(n):
+#     i = 1
+#     while i <= n:
+#         result = i * 10
+#         print(result)
+#         i += 1
+#
+#
+# pattern(2)
+
+def pattern(n):
+    i = 1
+    while i < n:
+        result = i ** 2
+        print(result)
+        i += 1
+
+
+pattern(7)
