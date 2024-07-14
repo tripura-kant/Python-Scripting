ebfe8ad9ecce3199246b22f356c96bc21531029d
Tripura kant <tripura.kant@zoom.us>
2024-07-15 00:09:10 +0530

    new 2024-07-15

--- a24/list/q4.py
+++ a24/list/q4.py
@@ -1,10 +1,20 @@
 # sum of all even number
-my_list = [5, 34, 98, 21, 29, 2]
+my_list = [54, 65, 321, 76876, 432, 65, 78, 54, 3454]
+
+n = len(my_list)
+sum = 0
+for i in my_list:
+    if i % 2 == 0:
+        sum = sum + i
+        # print(i)
+
+print(sum)
 
 n = len(my_list)
 sum = 0
 for i in range(0, n):
-    if i % 2 == 0:
+    if my_list[i] % 2 == 0:
         sum = sum + my_list[i]
+        # print(my_list[i])
 
 print(sum)
