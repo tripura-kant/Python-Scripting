764de9cb0351b5b7be6f1690c1c5808b513f3c5d
Tripura kant <tripura.kant@zoom.us>
2024-07-17 18:58:43 +0530

    new 2024-07-17

--- a24/list/ass7.py
+++ a24/list/ass7.py
@@ -1,4 +1,12 @@
-my_list = [5, 6, 7, 8]
+my_list = [5, 6, 7, 8, 4, 4, 12]
 
-my_list.extend([1, 2, 3])
+my_list.extend([1, 20, 3])
+# print(my_list)
+
+print(my_list.count(4))
+
+x = my_list.index(4)
+print(x)
+print(my_list.sort(reverse=True))
+print(my_list.sort(reverse=False))
 print(my_list)
---
+++ a24/list/question1.py
@@ -0,0 +1,20 @@
+"""
+Ask no from user = 6
+
+Enter element = 2
+Enter element = 2
+Enter element = 2
+Enter element = 2
+Enter element = 2
+Enter element = 2
+
+[2,2,2,2,2,2]
+"""
+n = int(input("Enter no: "))
+
+input_list = []
+for i in range(n):
+    num = input(f"Enter element {i + 1}: ")
+    input_list.append(num)
+
+print(input_list)
