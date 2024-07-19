b8a10058ba6939ec6d3c8760e52b5396960bf144
Tripura kant <tripura.kant@zoom.us>
2024-07-19 18:58:01 +0530

    new 2024-07-19

---
+++ a24/list/list_comprehension.py
@@ -0,0 +1,4 @@
+my_list = [i for i in range(1, 11)]
+
+my_list = [i for i in range(10, 0, -1)]
+print(my_list)
---
+++ a24/list/list_comprehension2.py
@@ -0,0 +1,4 @@
+my_list = [i for i in range(1, 11) if i % 2 == 0]
+
+# my_list = [i for i in range(10, 0, -1)]
+print(my_list)
