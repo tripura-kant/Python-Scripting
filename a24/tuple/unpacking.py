89a2ed24844bbdf6697bbca8162b0f2f2bc6db12
Tripura kant <tripura.kant@zoom.us>
2024-07-22 23:58:00 +0530

    new 2024-07-22

---
+++ a24/tuple/unpacking.py
@@ -0,0 +1,22 @@
+# num = int(input("Enter ur no    "))
+# # sqaure = num ** 2
+# # print(f"result is  {sqaure}")
+# # num
+# print(x ** 2 for x in num)
+
+# print(6 + 3)
+# print(6 - 3)
+# print(6 * 3)
+# print(6 / 3)
+
+# print("A")
+# print("B")
+# print("C")
+# print("D")
+# print("E")
+
+num1 = int(input())
+num2 = int(input())
+
+result = num1 - num2
+print(result)
