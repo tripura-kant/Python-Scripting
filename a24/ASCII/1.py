d9fd8281207679da6f09aa204c6f11feb38594ad
Tripura kant <tripura.kant@zoom.us>
2024-07-23 23:58:00 +0530

    new 2024-07-23

---
+++ a24/ASCII/1.py
@@ -0,0 +1,8 @@
+# ASCII
+my_char = "z"
+
+print(ord(my_char))
+
+x = 122
+
+print(chr(x))
---
+++ a24/ASCII/lower_count.py
@@ -0,0 +1,12 @@
+my_string = "asdasdasdasdskjhd*()&(*^*&%&^$"
+
+# count = 0
+# for ch in my_string:
+#     ascii_code = ord(ch)
+#     if 97 <= ascii_code <= 122:
+#         count += 1
+#
+# print(f"Total lowercase letters = {count}")
+
+for ch in my_string:
+    if ord("a") <= ord("ch") 
