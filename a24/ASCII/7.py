9b8ae4080e65de5e54ec15f322b364177f36c3ca
Tripura kant <tripura.kant@zoom.us>
2024-07-24 23:58:47 +0530

    new 2024-07-24

--- a24/ASCII/7.py
+++ a24/ASCII/7.py
@@ -1,11 +1,14 @@
 '''
 remove all symbol,digits, spaces from string
 '''
+#
+# my_string = "a%b@cadasd12_"
+#
+# result = ""
+#
+# for ch in my_string:
+#     if "a" <= ch <= "z":
+#
 
-my_string = "a%b@cadasd12_"
-
-result = ""
-
-for ch in my_string:
-    if "a" <= ch <= "z":
-        
+x = int(input(("enter no "))
+print(even for num in range(1, x + 1) if num % 2 == 0)
