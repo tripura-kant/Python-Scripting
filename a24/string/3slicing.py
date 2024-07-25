6013819fd23f6bcb6a11b27842765e27fc1f75f8
Tripura kant <tripura.kant@zoom.us>
2024-07-25 18:58:05 +0530

    new 2024-07-25

---
+++ a24/string/3slicing.py
@@ -0,0 +1,20 @@
+def longest_word(s: str) -> str:
+    current = ""
+    longest = ""
+
+    for char in my_string:
+        if char == " ":
+            if len(char) > len(longest):
+                longest = current
+            current = ""
+        else:
+            current += char
+
+    if len(current) > len(longest):
+        longest = current
+    return longest
+
+
+my_string = "Python is very easy language to learn"
+
+print(longest_word(my_string))
