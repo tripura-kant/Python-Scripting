503f072f36d30d276f3735b9480704c356a1dab6
Tripura kant <tripura.kant@zoom.us>
2024-07-26 22:58:01 +0530

    new 2024-07-26

--- scaler/3.py
+++ scaler/3.py
@@ -1,2 +1,8 @@
-amount = int(input("Please enter amount"))
-if amount % 
+amount = int(input("Please enter amount "))
+if amount >= 500 and amount % 100 == 0:
+    print("Discharging amount")
+else:
+    if amount < 500 and amount % 200 == 0:
+        print("Discharging amount")
+    else:
+        print("Not available")
