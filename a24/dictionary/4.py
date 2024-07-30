8910c05d93c49daff163c67663d01f130f6cdd4a
Tripura kant <tripura.kant@zoom.us>
2024-07-31 01:16:41 +0530

    new 2024-07-31

--- a24/dictionary/4.py
+++ a24/dictionary/4.py
@@ -1,6 +1,24 @@
 my_dict = {
-    "Anirudh"{
-    "age": 56,
-    "marks": []
+    "Anirudh": {
+        "age": 56,
+        "marks": [3, 40, 56]
+    },
+    "Muskan": {
+        "age": 16,
+        "marks": [13, 40, 56]
+    },
 }
-}
+# for name, info in my_dict.items():
+#     # print(info["age"])
+#     print(f'{name} has scored {sum(info["marks"])}')
+
+'''
+Anirudh has 56 marks
+Muskan has 100 marks
+'''
+
+for name, info in my_dict.items():
+    total = 0
+    for mark in info["marks"]:
+        total = total + mark
+    print(f'{name} has scored {total} marks')
