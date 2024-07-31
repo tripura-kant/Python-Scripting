7bfcc755d20aee72145c41f0418e270cee403669
Tripura kant <tripura.kant@zoom.us>
2024-07-31 15:58:39 +0530

    new 2024-07-31

--- a24/oops-class/1.py
+++ a24/oops-class/1.py
@@ -1,17 +1,22 @@
 class Student:
-    id = 0
+    idd = 0
     name = ""
     age = 30
     gender = ""
 
     def display(self):
         print(self)
-        print(f"ID = {self.id}")
+        print(f"ID = {self.idd}")
         print(f"name = {self.name}")
         print(f"age = {self.age}")
+        print(f"gender = {self.gender}")
 
 
 s1 = Student()
+s2 = Student()
 s1.name = "Tripura"
+s1.age = 99
+s1.gender = "Male"
 
-s1.display()
+# s1.display()
+s2.display()
