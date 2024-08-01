0e989d254b030806d8238109bb965d0ad4c36752
Tripura kant <tripura.kant@zoom.us>
2024-08-01 12:58:54 +0530

    new 2024-08-01

--- scaler/8.py
+++ scaler/8.py
@@ -2,13 +2,15 @@
 #     # YOUR CODE GOES HERE
 #     # Please take input and print output to standard input/output (stdin/stdout)
 #     # E.g. 'input()/raw_input()' for input & 'print' for output
-#     T = int(input())
+#     T = int(input("ENTER T "))
 #     for i in range(1, T + 1):
-#         n = int(input())
-#         result2 = str(n)[::-1]
-#         result = int(result2)
-#
-#         print(result)
+#         A = int(input("ENTER N1"))
+#         B = int(input("ENTER N2"))
+#         x = min(A, B)
+#         for i in range(x, 0, -1):
+#             if (A % i == 0 and B % i == 0):
+#                 print(i)
+#                 break
 #
 #
 # if __name__ == '__main__':
@@ -18,14 +20,15 @@ def main():
     # Please take input and print output to standard input/output (stdin/stdout)
     # E.g. 'input()/raw_input()' for input & 'print' for output
     T = int(input("ENTER T "))
+    return 0
     for i in range(1, T + 1):
         A = int(input("ENTER N1"))
         B = int(input("ENTER N2"))
-        x = min(A, B)
-        for i in range(x, 0, -1):
-            if (A % i == 0 and B % i == 0):
-                print(i)
-                break
+        #         x = min(A, B)
+        #         for i in range(x, 0, -1):
+        #             if (A % i == 0 and B % i == 0):
+        #                 print(i)
+        #                 break
 
 
 if __name__ == '__main__':
