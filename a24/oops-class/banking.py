fdb1ee97d7809746d3d5aa266d1fa2417faaa8be
Tripura kant <tripura.kant@zoom.us>
2024-08-03 23:42:36 +0530

    new 2024-08-03

--- a24/oops-class/banking.py
+++ a24/oops-class/banking.py
@@ -13,10 +13,73 @@ Menu
 
 
 class Banking:
-    def __init__(self, acc_no, name, account_type, balance, branch):
+    def __init__(self, acc_no, name, account_type, branch):
         self.acc_no = acc_no
         self.name = name
         self.account_type = account_type
-        self.balance = balance
+        self.balance = 0
         self.branch = branch
-    
+
+    def get_balance(self):
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance = amount
+        return amount
+
+    def display(self):
+        print(f"acc_no = {self.acc_no}")
+        print(f"Name = {self.name}")
+        print(f"account_type = {self.account_type}")
+        print(f"balance = {self.balance}")
+        print(f"branch = {self.branch}\n\n")
+
+
+bank_details = []
+while True:
+    print("1) Add a account")
+    print("2) Display all account")
+    print("3) Exit")
+    print("4) Get balance")
+    print("5) Deposit")
+
+    choice = int(input("Enter your choice = "))
+    if choice == 1:
+        acc_no = int(input("Enter acc_no = "))
+        name = input("Enter name = ")
+        account_type = input("Enter account_type = ")
+        branch = input("Enter branch = ")
+
+        x = Banking(acc_no, name, account_type, branch)
+        bank_details.append(x)
+
+    elif choice == 2:
+        if len(bank_details) == 0:
+            print("No account found")
+        else:
+            for acc in bank_details:
+                acc.display()
+
+    elif choice == 4:
+        acc_no = int(input("ENter acc no"))
+        for acc in bank_details:
+            if acc.acc_no == acc_no:
+                print(f"Your balace is {acc.get_balance()}")
+
+    elif choice == 5:
+        acc_no = int(input("ENter acc no"))
+        amount = int(input("Enter deposit amount"))
+        for acc in bank_details:
+            if acc.acc_no == acc_no:
+                acc.deposit(amount)
+                print(f"Your balace is {acc.get_balance()}")
+
+
+
+
+
+    elif choice == 3:
+        print("Exiting bank application    ")
+        break
+    else:
+        print("Invalid Choice")
