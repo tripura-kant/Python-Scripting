aa17406fb37fb8341b3ed1b75ab4deb4175e7181
Tripura kant <tripura.kant@zoom.us>
2024-08-03 21:58:37 +0530

    new 2024-08-03

--- scaler/3aug.py
+++ scaler/3aug.py
@@ -1,28 +1,14 @@
-# x = 55
-#
-#
-# def hi():
-#     return x
-#
-#
-# x += 10
-# print(hi())
-#
-# Iterate to find all the factors of given number n from 1 to (n // 2) + 1, the number would have no factors beyond this.
-# A number is the factor of n if it completely divides it without any remainder.
-# Take the sum of all factors of the given number n.
-
-def perfect_number(n):
-    if n <= 1:
-        return 0
-    sum_of_divisors = 0
-    for i in range(1, n // 2 + 1):
-        if n % i == 0:
-            sum_of_divisors += i
-    if sum_of_divisors == n:
-        return 1
-    else:
-        return 0
+def pattern(N):
+    for i in range(1, N + 1):
+        for j in range(i, N + 1):
+            if i == 1 or j == i or j == N:
+                print("j", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
 
 
-print(perfect_number(6))
+# Driver code
+if __name__ == "__main__":
+    N = 6
+    pattern(N)
