9e90608eaccd69a9476a71f5980e76967b0a179c
Tripura kant <tripura.kant@zoom.us>
2024-08-06 22:58:13 +0530

    new 2024-08-06

--- scaler/11.py
+++ scaler/11.py
@@ -1,15 +1,15 @@
-def shift():
-    n = 5  # Read the number of elements
-    a = list(map(int, input().split()))  # Read the list of integers
-
-    # Create a new list with the last element at the beginning
-    shifted_a = [a[-1]] + a[:-1]
-
-    # Print the elements with proper spacing
-    for i in range(n):
-        print(shifted_a[i], end=" ")  # Print the current element
-
-
-# Example usage
-if __name__ == "__main__":
-    shift()
+# def shift():
+#     n = 5  # Read the number of elements
+#     a = list(map(int, input().split()))  # Read the list of integers
+#
+#     # Create a new list with the last element at the beginning
+#     shifted_a = [a[-1]] + a[:-1]
+#
+#     # Print the elements with proper spacing
+#     for i in range(n):
+#         print(shifted_a[i], end=" ")  # Print the current element
+#
+#
+# # Example usage
+# if __name__ == "__main__":
+#     shift()
