8915021b030f0b39cffd5d850e62d57d0776a84d
Tripura kant <tripura.kant@zoom.us>
2024-08-13 10:58:08 +0530

    new 2024-08-13

--- scaler/13aug.py
+++ scaler/13aug.py
@@ -1,23 +1,11 @@
-def set_operation(sent1, sent2):
-    words1 = sent1.split()
-    words2 = sent2.split()
-    print(words1)
-    print(words2)
-
-    a = set(words1)
-    b = set(words2)
-
-    total_unique_count = len(a) + len(b)
-
-    return total_unique_count
+def unique_count(tup):
+    lst = []
+    for v in tup:
+        # We got v first time
+        if v not in lst:
+            lst.append(v)
+            print(v, ':', tup.count(v))
 
 
-# Example usage
-sentence1 = "in data analysis we use data and process it further to create better interpreted data"
-sentence2 = "more and more data will be passively collected"
-
-# Get result
-total_unique_count = set_operation(sentence1, sentence2)
-
-# Print result
-print(f"{total_unique_count}")
+tup = (100, -10, -10, "ONE", [22], [22], "ONE")
+unique_count(tup)
