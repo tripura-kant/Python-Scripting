29dd37280223455b99e77772aba4304600d5605b
Tripura kant <tripurakant@gmail.com>
2024-08-16 01:58:45 +0530

    new 2024-08-16

---
+++ scaler/16ug.py
@@ -0,0 +1,22 @@
+# def main():
+#     # YOUR CODE GOES HERE
+#     # Please take input and print output to standard input/output (stdin/stdout)
+#     # E.g. 'input()/raw_input()' for input & 'print' for output
+#     T=2
+#     for i in range(0, T+1):
+#         rev =
+#
+#
+#
+#     return 0
+#
+# if __name__ == '__main__':
+#     main()
+#
+# T=1
+# N=100
+
+num = "234"
+
+for i in num:
+    print(i)
