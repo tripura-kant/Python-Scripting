4de20eaa24138c32b12c5da500ea7301f5c44715
Tripura kant <tripurakant@gmail.com>
2024-08-26 13:58:26 +0530

    new 2024-08-26

--- scaler/array/array-rev.py
+++ scaler/array/array-rev.py
@@ -1,3 +1,17 @@
-arr = [1, 3, 2, 9, 0]
-
 # print(arr[::-1])
+
+def rev_array(arr):
+    start = 0
+    end = len(arr) - 1
+
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+
+        start += 1
+        end -= 1
+
+    return arr
+
+
+arr = [1, 3, 2, 9, 0]
+print(rev_array(arr))
