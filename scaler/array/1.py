bc944d4038c68d05d20a25cfc5345dc0845410c2
Tripura kant <tripurakant@gmail.com>
2024-08-27 20:58:56 +0530

    new 2024-08-27

---
+++ scaler/array/1.py
@@ -0,0 +1,19 @@
+# Given an array A and an integer B.
+# A pair(i, j) in the array is a good pair
+# if i != j and (A[i] + A[j] == B).
+# Check if any good pair exist or not.
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        for i in range(len(A):
+            if (A[i] + A[j] == B):
+
+
+
+
+
+A = [1, 2, 3, 4]
+B = 7
