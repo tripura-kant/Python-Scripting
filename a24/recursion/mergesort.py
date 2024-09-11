cd60649cbd67ed6857606688c055dd537fa6b0a5
Tripura kant <tripurakant@gmail.com>
2024-09-11 11:58:58 +0530

    new 2024-09-11

--- a24/recursion/mergesort.py
+++ a24/recursion/mergesort.py
@@ -4,4 +4,23 @@ def merge(arr1, arr2):
     j = 0
     while i < len(arr1) and j < len(arr2):
         if arr1[i] <= arr2[j]:
-            
+            merged.append(arr1[i])
+            i += 1
+        else:
+            merged.append(arr2[j])
+            j += 1
+    while i < len(arr1):
+        merged.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        merged.append(arr2[j])
+        j += 1
+
+    return merged
+
+
+arr1 = [1, 1, 2, 3, 4]
+arr2 = [2, 4, 5, 6]
+
+print(merge(arr1, arr2))
