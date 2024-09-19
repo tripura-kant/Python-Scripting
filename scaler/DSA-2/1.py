59ead528c9216f2fc0490fdbb4f5c579e75ade39
Tripura kant <tripurakant@gmail.com>
2024-09-19 12:58:55 +0530

    new 2024-09-19

--- scaler/DSA-2/1.py
+++ scaler/DSA-2/1.py
@@ -1,8 +1,16 @@
 # Find sum of all subarray sums
+def subarray_sums(arr):
+    total = 0
+    n = len(arr)
+    for i in range(n):
+        for j in range(i, n):
+            sum = 0
+            for k in range(i, j + 1):
+                sum += arr[k]
+            total += sum
 
-for i in range(n):
-    for j in range(i, n):
-        sum = 0
-        for k in range(i, j + 1):
-            sum += arr[k]
-           
+    return total
+
+
+arr = [1, 2, 3]
+print(subarray_sums(arr))
