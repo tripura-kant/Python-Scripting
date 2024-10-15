73717026ea4bd354c092bc81c459d1d718cf9a48
Tripura kant <tripurakant@gmail.com>
2024-10-15 22:58:01 +0530

    new 2024-10-15

--- scaler/hashing/2.py
+++ scaler/hashing/2.py
@@ -1,12 +1,28 @@
-# Hash set
-my_set = set()
+# # Hash set
+# my_set = set()
+#
+# my_set.add(5)
+# my_set.add(649)
+#
+# print(my_set)
+#
+# # search
+#
+# if 649 in my_set:
+#     print("24 is present")
 
-my_set.add(5)
-my_set.add(649)
 
-print(my_set)
+# mymap = {}
+# mymap[1] = 4
+# if 1 in mymap:
+#     print("Yes")
+# if 2 not in mymap:
+#     print("YESS!")
 
-# search
+mymap = {}
+mymap[1] = 4
+mymap[3] = 5
 
-if 24 in my_set:
-    print("24 is present")
+for key in mymap:
+    print(key, end=', ')
+    print(mymap[key])
