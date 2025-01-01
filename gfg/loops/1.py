6c2aa0b99af8424a9f20a091871349990297a810
Tripura kant <tripurakant@gmail.com>
2025-01-01 21:58:29 +0530

    new 2025-01-01

--- gfg/loops/1.py
+++ gfg/loops/1.py
@@ -1,6 +1,13 @@
-x = [1, 2]
-y = [3, 5]
+# x = [1, 2]
+# y = [3, 5]
+#
+# for i in x:
+#     for j in y:
+#         print(i, j)
 
-for i in x:
-    for j in y:
-        print()
+
+for i in range(2, 4):
+    for j in range(1, 11):
+        print(i, "*", j, "=", i * j)
+
+    print()
