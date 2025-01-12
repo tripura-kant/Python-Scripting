93b75ce5914a26c716efa19624aa3571c9dff375
Tripura kant <tripurakant@gmail.com>
2025-01-13 00:58:32 +0530

    Update on 2025-01-13

--- scaler/DSA1/class-1-intro-to-array/3.py
+++ scaler/DSA1/class-1-intro-to-array/3.py
@@ -1,1 +1,16 @@
 # Rotate k times array
+arr = [1, 2, 3, 4, 5]
+k = 2
+n = len(arr)
+
+
+def rotate(arr, n, k):
+    for i in range(k):  # rotate k times
+        temp = arr[n - 1]  # last element
+        for j in range(n - 1, 0, -1):
+            arr[j] = arr[j - 1]
+        arr[0] = temp
+
+
+rotate(arr, n, k)
+print(arr)
