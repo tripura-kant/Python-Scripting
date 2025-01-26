17d58d3ca40ec9f7474703e19396255a9884f30f
Tripura kant <tripurakant@gmail.com>
2025-01-26 20:58:00 +0530

    Update on 2025-01-26

--- Scaler/DSA-1/class-4/optimizesumofallsubarray.py
+++ Scaler/DSA-1/class-4/optimizesumofallsubarray.py
@@ -2,11 +2,11 @@ def sumofallsubarrays(arr, n):
     total = 0
     for s in range(n):
         for e in range(s, n):
-            subarraysum = 0
-            for k in range(s, e+1):
-                subarraysum  += arr[k]
+            currentsum = 0
+            for e in range(s, e+1):
+                currentsum  += arr[e]
 
-            total += subarraysum
+            total += currentsum
 
     print(total)
 
