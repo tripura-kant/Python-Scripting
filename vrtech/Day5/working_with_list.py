b32f997f128006a1b739ddb6693a0d261653e8c9
Tripura kant <tripurakant@gmail.com>
2025-02-12 14:58:20 +0530

    Update on 2025-02-12

--- vrtech/Day5/working_with_list.py
+++ vrtech/Day5/working_with_list.py
@@ -1,5 +1,27 @@
-myClouds=['aws', 'azure', 'google', 'ibm']
+myDevOpsTools={
+    "integraionTool": "jenkins",
+    "configTool"   : "ansible",
+    "dbs" : ["oracledb", "mariadb",]
+}
 
-print(len(myClouds))
+# print(myDevOpsTools["dbs"])
+# print(myDevOpsTools.get("dbs","value is not defined"))
 
-print('aws' in myClouds)
+# print(list(myDevOpsTools.keys()))
+# print(list(myDevOpsTools.values()))
+# print(list(myDevOpsTools.items()))
+
+print(myDevOpsTools.get("dbs")[1])
+
+# print("configTool" in myDevOpsTools.keys()) #print("configTool" in myDevOpsTools)
+# print("ansible" in myDevOpsTools.values())
+
+
+# jsonResponse={
+#     "tools": [ { "cloud": ["aws", "azure"]}, {"container":['docker',"k8s"]} ],
+#     "version": "1.0"
+# }
+
+# # toolslist=jsonResponse.get('tools')
+# # print(toolslist[0])
+# print(jsonResponse['tools'][0].get("cloud")[0])
