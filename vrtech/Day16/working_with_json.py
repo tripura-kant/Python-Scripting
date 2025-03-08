6ec5a0c1d06aac851efdb1394f8f55f53f4b7ea6
Tripura kant <tripurakant@gmail.com>
2025-03-08 21:58:37 +0530

    Update on 2025-03-08

--- vrtech/Day16/working_with_json.py
+++ vrtech/Day16/working_with_json.py
@@ -1,3 +1,10 @@
-with open('sample.csv', 'r') as file:
-    for eachline in file:
-        print(eachline.strip().split(',')[0])
+# with open('sample.csv', 'r') as file:
+#     for eachline in file:
+#         print(eachline.strip().split(',')[0])
+
+import csv
+
+with open("sample.csv", "r") as file:
+    reader = csv.reader(file, delimiter="|")
+    for eachrow in reader:
+        print(eachrow)
