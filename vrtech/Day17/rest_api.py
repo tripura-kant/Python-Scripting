e84d8518153f49dfcfb9e4cb9f963468c0c34642
Tripura kant <tripurakant@gmail.com>
2025-03-09 10:58:15 +0530

    Update on 2025-03-09

---
+++ vrtech/Day17/rest_api.py
@@ -0,0 +1,16 @@
+import sys
+
+try:
+    import requests
+except ModuleNotFoundError:
+    print(f'Install request module and try again')
+    sys.exit(1)
+
+APIURL = 'https://jsonplaceholder.typicode.com/post/'
+
+response = requests.get(url=APIURL, verify=True)
+st_code = response.status_code
+if st_code == 200:
+    print(f"Successfully executed get method on api and status code is : {st_code}")
+else:
+    print(f'failed and status code is: {st_code}')
