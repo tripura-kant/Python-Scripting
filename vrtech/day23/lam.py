ddb74d80b809fd61c898c100a47738de3ff9d63e
Tripura kant <tripurakant@gmail.com>
2025-03-20 14:58:01 +0530

    Update on 2025-03-20

---
+++ vrtech/day23/lam.py
@@ -0,0 +1,3 @@
+getmax = lambda input1: max(input1)
+mylist = [1, 2, 3]
+print(getmax(mylist))
---
+++ vrtech/day23/oops.py
@@ -0,0 +1,16 @@
+class calculator(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def addition(self, x, y):
+        return x + y
+
+    def substraction(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+
+mycalculator = calculator
